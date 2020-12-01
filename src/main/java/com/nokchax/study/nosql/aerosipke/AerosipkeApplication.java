661d3ecc8afbb99c77850472876e538065781ec7
nokchax <nokchax@gmail.com>
2020-12-01 20:28:27 +0900

    init project

---
+++ src/main/java/com/nokchax/study/nosql/aerosipke/AerosipkeApplication.java
@@ -0,0 +1,13 @@
+package com.nokchax.study.nosql.aerosipke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerosipkeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AerosipkeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nokchax/study/nosql/aerosipke/AerosipkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nokchax.study.nosql.aerosipke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerosipkeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
