47384d06c40902825e5a51d21ab41c36dea1ee3a
nokchax <nokchax@gmail.com>
2020-12-06 23:28:33 +0900

    chore: fix misprint

--- src/main/java/com/nokchax/study/nosql/aerosipke/AerospikeApplication.java
+++ src/main/java/com/nokchax/study/nosql/aerosipke/AerospikeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AerosipkeApplication {
+public class AerospikeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AerosipkeApplication.class, args);
+        SpringApplication.run(AerospikeApplication.class, args);
     }
 
 }
