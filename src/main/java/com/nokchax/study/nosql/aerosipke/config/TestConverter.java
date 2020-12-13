9ef92ceb19da1406fd95983dfc28d9a21620479e
nokchax <nokchax@gmail.com>
2020-12-13 21:17:00 +0900

    feat: update converter

--- src/main/java/com/nokchax/study/nosql/aerosipke/config/TestConverter.java
+++ src/main/java/com/nokchax/study/nosql/aerosipke/config/TestConverter.java
@@ -1,35 +1,56 @@
 package com.nokchax.study.nosql.aerosipke.config;
 
+import com.aerospike.client.Bin;
+import com.aerospike.client.Key;
 import com.nokchax.study.nosql.aerosipke.domain.TestDomain;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.aerospike.convert.AerospikeReadData;
+import org.springframework.data.aerospike.convert.AerospikeWriteData;
 import org.springframework.data.convert.ReadingConverter;
 import org.springframework.data.convert.WritingConverter;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 public class TestConverter {
+
     @WritingConverter
-    public enum UserDataToMapConverter implements Converter<TestDomain, Map<String, Object>> {
-        INSTANCE;
+    @RequiredArgsConstructor
+    public static class UserDataToMapConverter implements Converter<TestDomain, AerospikeWriteData> {
+        private final String namespace = "test";
+        private final String setName = "test";
 
         @Override
-        public Map<String, Object> convert(TestDomain source) {
+        public AerospikeWriteData convert(TestDomain source) {
+            Key key = new Key(namespace, setName, source.getId());
+            int expire = 2;
+
             Map<String, Object> map = Map.of(
                     "id", source.getId(),
                     "value", source.getValue()
             );
-            return map;
+
+            Integer version = null; // not versionable document
+            Collection<Bin> bins = List.of(
+                    new Bin("id", source.getId()),
+                    new Bin("value", source.getValue())
+            );
+
+            return new AerospikeWriteData(key, bins, expire, version);
         }
     }
 
     @ReadingConverter
-    public enum MapToUserDataToConverter implements Converter<Map<String, Object>, TestDomain> {
+    public enum MapToUserDataToConverter implements Converter<AerospikeReadData, TestDomain> {
         INSTANCE;
 
         @Override
-        public TestDomain convert(Map<String, Object> source) {
-            long id = (long) source.getOrDefault("id", 0);
-            String value = (String) source.getOrDefault("value", null);
+        public TestDomain convert(AerospikeReadData source) {
+            long id = (long) source.getValue("id");
+            String value = (String) source.getValue("value");
+
             return new TestDomain(id, value);
         }
     }
